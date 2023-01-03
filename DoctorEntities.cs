51757ad90602fe0bbe6387d0e2e563b389998cea
PratikshaSule <121611202+PratikshaSule@users.noreply.github.com>
2023-01-03 16:59:20 +0530

    Add files via upload

---
+++ DoctorEntities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PatientManagementSystemEntities;
+
+namespace PatientManagementSystemEntities
+{
+    public class DoctorEntities
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DoctorID { get; set; }
+        public string Gender { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Profession{ get; set; }
+        public string Education { get; set; }
+    }
+}
---
+++ PatientEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PatientManagementSystemEntities
+{
+    public class PatientEntities
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Gender { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+    }
+}
